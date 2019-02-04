4e84b899affd50de8124e7548cb5e6dce87cc5c7
SOS1617_rvs <rvizuetes@gmail.com>
2019-02-04 23:01:32 +0000

    Inicialización del html

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var express = require("express");
-var port = (process.env.PORT || 16778);
-var app = express();
 var path = require('path');
-var publicFolder = path.join(__dirname, 'public');
 
-app.listen(port,(err) => {
-    if(!err)
-        console.log("Server initialized on port " + port);
-    else
-        console.log("ERROR initializing server on port " + port + ": " + err);
+var app = express();
+var port = (process.env.PORT || 16778);
+
+var publicFolder = path.join(__dirname, "public");
+
+app.use("/", express.static(publicFolder));
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello");
 });
 
-app.get("/",(req,res)=>{
-    res.send("<html><body><h1>HOLA</h1></body></html>");
+app.listen(port,()=>{
+   console.log("Server running in port " + port); 
+}).on("error",(e)=>{
+    console.log("Server cannot be started: "+e);
+    process.exit(1);
 });
+
+
+
